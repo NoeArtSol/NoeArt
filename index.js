ca45b77f9b9b7423bfecbd0179759594e2a68c80
NoeArtSol <isoldevila.g@gmail.com>
2025-02-09 20:13:56 +0100

    index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import NoeArtGallery from "./NoeArtGallery";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <NoeArtGallery />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
